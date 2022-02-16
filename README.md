d6f21b6efc5045800fa3e4e86d2ea5ffb8496ff7
8columns <8columns@gmail.com>
2022-02-17 06:24:11 +0700

    first commit

--- README.md
+++ README.md
@@ -1,35 +1,57 @@
-# jquery-lite
-custom version of jquery
+# jquery-lite (forked version)
 
-# Usage
+Custom version of jquery with limited functions and very light weight. (only 211 lines of code). This is a fored version from the original code by **Minseok Kim**.
+
+You can visit the original version by **Minseok Kim** at https://github.com/mskims/jquery-lite
+
+
+# File Size
+- jquery-lite.js (4.44KB)
+- jquery-lite.min.js (2.11KB) (961 bytes when zipped)
+
+
+# What I have changes
+
+- change from classic function to arrow style (to be more concised)
+- move some functions to __proto__ (to be more jQuery's way)
+- add get function to element (to be more jQuery's way)
+- modify data function to use dataset (original code use attribute)
+- allow setting css using object (original code not yet implemented)
+- allow using $() around function (to be more jQuery's way) 
+
+# Usages
 
 ```js
-window.document.addEventListener('DOMContentLoaded', function() {
+$(() => {
+    // set css using object syntax
+    $("h1").css{
+            color: 'red',
+            textDecoration: 'underline'
+        }
+    )
 
-    $(".select").each(function(value, index){
-        console.log(this.e, $(this).html());
-    });
+    // bind to click event
+    $("a").click(function(e) {
+        console.log('I am clicked!');
+    })
 
-    $(".select").click(function(e){
-        this.toggleClass("active");
-    }).find(".options li").click(function(){
-        this.html();
-        console.log(this.data("value"));
-    });
+    // add class
+    $("p").addClass('red-text');
 
+    // remove class
+    $("p").removeClass('red-text');
 
-    $(".lazy_img").each(function(){
-        this.attr('src', this.data('original'));
-    });
+    // toggle class
+    $("p").toggleClass('red-text');
 
-    $(".btn-close-option-layer").click(function(){
-        console.log("!");
-        // this.parent().attr("style", "bottom: -100%");
-        this.parent().css("bottom", "-100%");
-    });
+    // set attribute
+    $("a").attr("title", "link to Google");
 
+    // set data
+    $("a").data("x", 10);
 });
 ```
 
 # Author
-maybe me?
+
+This is a forked version with small modification from the original code by **Minseok Kim** (https://github.com/mskims/jquery-lite)
--- jquery-lite.js
+++ jquery-lite.js
@@ -1,197 +1,211 @@
-(function(){
-    var $ = function(sel, ne) {
-        var e = null;
-        var _ = this;
-        _.init = function(_sel, ne){
-            if(typeof ne != 'undefined'){
-                _.e = ne;
-            }else if(typeof _sel == 'string'){
-                _.e = document.querySelectorAll(_sel);
-            }else if(_.isDom(_sel)){
-                console.log("돔이네!");
-                _.e = _sel;
-            }
-        };
+(() => {
+  var $ = (sel) => {
+    var e = null
+    var _ = {}
 
+    const _camel_to_snake = (string) => {
+      return string.replace(/([a-z]|(?=[A-Z]))([A-Z])/g, "$1-$2").toLowerCase()
+    }
 
-        // Iter
-        _.each = function(func){
-            /**
-             * $("SEL").each(function);
-             */
-            for(var i = 0, j = _.e.length ; i < j; i ++ ){
-                func.apply(new $(null, [_.e[i]]), [_.e[i], i]);
-            }
-        };
+    const _is_dom = (o) => {
+      return typeof HTMLElement === "object" ? o instanceof HTMLElement : o && typeof o === "object" && o !== null && o.nodeType === 1 && typeof o.nodeName === "string"
+    }
 
-        _.loop = function(func){
-            for(var i = 0, j = _.e.length ; i < j; i ++ ){
-                func( _.e[i] );
-            }
-        };
+    _.__proto__.init = (_sel) => {
+      _.__proto__.length = 0
 
-        _.loopIter = function(iter, func){
-            for(var i = 0, j = iter.length ; i < j; i ++ ){
-                func( iter[i] );
-            }
-        };
+      if (typeof _sel === "string") {
+        _.e = document.querySelectorAll(_sel)
+        _.length = _.e.length
+        _.e.forEach((item, i) => {
+          _[i] = item
+        })
+      } else if (typeof _sel === 'function') { 
+        window.onload = _sel
+      } else if (_is_dom(_sel)) {
+        _.e = _sel
+        _[0] = _sel
+        _.length = 1
+      }
+    }
 
+    _.__proto__.get = (idx) => {
+      return _[idx]
+    }
 
-        // CLASS
-        _.addClass = function(className){
-            _.loop(function(el){
-                el.classList.add(className);
-            });
+    _.__proto__.each = (func) => {
+      for (var i = 0, j = _.e.length; i < j; i++) {
+        func.apply(new $(null, [_.e[i]]), [_.e[i], i])
+      }
+    }
 
-            return _;
-        };
+    _.__proto__.loop = (func) => {
+      for (var i = 0, j = _.e.length; i < j; i++) {
+        func(_.e[i])
+      }
+    }
 
-        _.removeClass = function(clsssName){
-            _.loop(function(el){
-                el.classList.remove(className);
-            });
+    _.__proto__.loopIter = (iter, func) => {
+      for (var i = 0, j = iter.length; i < j; i++) {
+        func(iter[i])
+      }
+    }
 
-            return _;
-        };
+    _.__proto__.addClass = (className) => {
+      _.loop(function (el) {
+        el.classList.add(className)
+      })
 
-        _.toggleClass = function(className){
-            _.loop(function(el){
-                el.classList.toggle(className);
-            });
-        };
+      return _
+    }
 
+    _.__proto__.removeClass = (clsssName) => {
+      _.loop(function (el) {
+        el.classList.remove(className)
+      })
 
+      return _
+    }
 
-        // CSS
-        _.css = function(pro, val){
-            if(typeof val != 'undefined'){
-                _.loop(function(el){
-                    el.style[pro] = val;
-                });
-            }else{
-                alert("NOT YET!");
-            }
-            return _;
-        };
+    _.__proto__.toggleClass = (className) => {
+      _.loop(function (el) {
+        el.classList.toggle(className)
+      })
+    }
 
+    _.__proto__.css = (pro, val) => {
+      if (typeof pro === "object") {
+        const _style = {}
+        const arr = Object.keys(pro)
 
-        // DOM
-        _.parent = function(){
-            var ar = [];
-            _.loop(function(el){
-                ar.push(el.parentNode);
-            });
-            _.set(ar);
+        arr.forEach((item) => {
+          _style[_camel_to_snake(item)] = pro[item]
+        })
 
-            return _;
-        };
+        _.loop(function (el) {
+          Object.keys(_style).forEach((item) => {
+            el.style[item] = _style[item]
+          })
+        })
+      } else {
+        if (typeof val != "undefined") {
+          _.loop(function (el) {
+            el.style[pro] = val
+          })
+        }
+      }
 
-        _.remove = function(){
-            _.loop(function(el){
-                el.remove();
-            });
+      return _
+    }
 
-            return _;
-        };
+    _.__proto__.parent = () => {
+      var ar = []
+      _.loop(function (el) {
+        ar.push(el.parentNode)
+      })
+      _.set(ar)
 
-        _.find = function(selToF){
-            var ar = [];
-            _.loop(function(el){
-                var ar2 = [];
+      return _
+    }
 
-                _.loopIter(el.querySelectorAll(selToF), function(elF){
-                    ar.push(elF);
-                });
-            });
-            _.set(ar);
-            return _;
-        };
+    _.__proto__.remove = () => {
+      _.loop(function (el) {
+        el.remove()
+      })
 
-        _.eq = function(i){
-            _.set([_.e[i]]);
-            return _;
-        };
+      return _
+    }
 
+    _.__proto__.find = (selToF) => {
+      var ar = []
+      _.loop(function (el) {
+        var ar2 = []
 
-        // EVENT
-        _.addEvent = function(trigger, func){
-            _.loop(function(el){
-                el.addEventListener(trigger, function(e){
-                    func.apply(new $(null, [el]), [e]);
-                });
-            });
-            return _;
-        };
+        _.loopIter(el.querySelectorAll(selToF), (elF) => {
+          ar.push(elF)
+        })
+      })
+      _.set(ar)
+      return _
+    }
 
-        _.on = function(trigger, func){
-            return _.addEvent(trigger, func);
-        };
+    _.__proto__.eq = (i) => {
+      _.set([_.e[i]])
+      return _
+    }
 
-        _.click = function(func){
-            _.addEvent('click', func);
-            return _;
-        };
+    _.__proto__.addEvent = (trigger, func) => {
+      _.loop(function (el) {
+        el.addEventListener(trigger, (e) => {
+          func.apply(new $(null, [el]), [e])
+        })
+      })
+      return _
+    }
 
+    _.__proto__.on = (trigger, func) => {
+      return _.addEvent(trigger, func)
+    }
 
-        // DATA
-        _.html = function(ne){
-            if(typeof ne != 'undefined'){
-                _.loop(function(el){
-                    el.innerHTML = ne;
-                });
-                return _;
-            } else {
-                return _.e[0].innerHTML;
-            }
-        };
+    _.__proto__.click = (func) => {
+      _.addEvent("click", func)
+      return _
+    }
 
-        _.text = function(ne){
-            if(typeof ne != 'undefined'){
-                _.loop(function(el){
-                    el.innerHTML = ne;
-                });
-                return _;
-            } else {
-                return _.e[0].textContent;
-            }
-        };
+    _.__proto__.html = (ne) => {
+      if (typeof ne != "undefined") {
+        _.loop(function (el) {
+          el.innerHTML = ne
+        })
+        return _
+      } else {
+        return _.e[0].innerHTML
+      }
+    }
 
-        _.val = function(){
-            return _.e[0].value;
-        };
+    _.__proto__.text = (ne) => {
+      if (typeof ne != "undefined") {
+        _.loop(function (el) {
+          el.innerHTML = ne
+        })
+        return _
+      } else {
+        return _.e[0].textContent
+      }
+    }
 
-        _.data = function(prop){
-            return _.e[0].getAttribute('data-'+prop);
-        };
+    _.__proto__.val = () => {
+      return _.e[0].value
+    }
 
-        _.attr = function(prop, ne){
-            if(typeof ne != 'undefined'){
-                _.loop(function(el){
-                    el.setAttribute(prop, ne);
-                });
-                return _;
-            }else{
-                return _.e[0].getAttribute(prop);
-            }
+    _.__proto__.data = (prop, val) => {
+      if (val === undefined) {
+        return _.e[0].dataset[prop]
+      } else {
+        _.e[0].dataset[prop] = val
+        return _
+      }
+    }
 
+    _.__proto__.attr = (prop, ne) => {
+      if (typeof ne != "undefined") {
+        _.loop(function (el) {
+          el.setAttribute(prop, ne)
+        })
+        return _
+      } else {
+        return _.e[0].getAttribute(prop)
+      }
+    }
 
-        };
+    _.__proto__.set = (el) => {
+      _.e = el
+    }
+    _.__proto__.init(sel)
+    return _
+  }
 
-        _.set = function(el){
-            _.e = el;
-        };
-
-        // CHECK
-        _.isDom = function (o) {
-            return typeof HTMLElement === "object" ? o instanceof HTMLElement : o && typeof o === "object" && o !== null && o.nodeType === 1 && typeof o.nodeName==="string";
-        };
-
-
-        _.init(sel, ne);
-        return _;
-    };
-
-    window.$ = $;
-    window.jQuery = $;
-    return $;
-})();
+  window.$ = $
+  window.jQuery = $
+  return $
+})()
